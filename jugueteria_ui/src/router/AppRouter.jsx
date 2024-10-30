d98117fbb2ee11db8c91ed609d3dbc0996d006d3
Anett Yomali Vera Carbajal <132587180+AnettVera@users.noreply.github.com>
2024-10-30 10:09:25 -0600

    Configuración inicial

---
+++ jugueteria_ui/src/router/AppRouter.jsx
@@ -0,0 +1,8 @@
+import React, {useContext} from "react";
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider,} from 'react-router-dom';
+
+const AppRouter=()=>{
+    
+}
+
+export default AppRouter;
