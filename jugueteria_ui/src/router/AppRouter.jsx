a636a364feeb8a6d2ef9e428986868f7735d4314
AnettVera <20223tn034@utez.edu.mx>
2024-10-30 10:37:56 -0600

    configuracion inicial

--- jugueteria_ui/src/router/AppRouter.jsx
+++
@@ -1,8 +0,0 @@
-import React, {useContext} from "react";
-import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider,} from 'react-router-dom';
-
-const AppRouter=()=>{
-    
-}
-
-export default AppRouter;
