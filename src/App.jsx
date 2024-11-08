b5f96d6c5591aa62144b4e14c9518f1881cbef08
AnettVera <20223tn034@utez.edu.mx>
2024-11-08 11:49:43 -0600

    Modos claro y oscuro

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,12 @@ import Header from './components/Elements/Generales/Header';
 import Footer from './components/Elements/Generales/Footer';
 import ProductCard from './components/Elements/Generales/ProductCard';
 import './output.scss'
-import SignInPage from './modules/auth/SignInPage';
-import RegisterPage from './modules/auth/RegisterPage'
-
 function App() {
-const [isAuthenticated, setIsAuthenticated] = useState(true);
- return(
-  <div>
- 
-   
-  </div>
- );
+  return (
+    <ThemeProvider>
+      
+    </ThemeProvider>
+  );
 }
 
 export default App;
