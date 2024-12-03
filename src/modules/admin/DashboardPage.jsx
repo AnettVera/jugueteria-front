db07fcf80a444f54b825a83b174e9524c9af0c96
Leo-Noriega <leonoriega100@gmail.com>
2024-12-02 20:33:00 -0600

    feat: Dyanmic stock of the products in the db

--- src/modules/admin/DashboardPage.jsx
+++ src/modules/admin/DashboardPage.jsx
@@ -1,30 +1,50 @@
-import React from 'react'
-import CardPieChart from './../../components/Admin/CardPieChart'
-import SellingProducts from '../../components/Admin/SellingProducts'
-import './../../assets/Pages/admin_pages/Dashboard.scss'
+import React, { useEffect, useState } from 'react';
+import CardPieChart from './../../components/Admin/CardPieChart';
+import SellingProducts from '../../components/Admin/SellingProducts';
+import './../../assets/Pages/admin_pages/Dashboard.scss';
+import axios from 'axios';
+
 const DashboardPage = () => {
+  const [stockData, setStockData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchStockData = async () => {
+      try {
+        const response = await axios.get('http://localhost:6868/toystore/products/totalStock');
+        setStockData(response.data);
+      } catch (error) {
+        console.error('Error fetching stock data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStockData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className='dashboard'>
       <p>DASHBOARD</p>
-     <div className='dashboard__content'>
-        <CardPieChart name='Construcción' cantidad={100}/>
-        <CardPieChart name='Peluches' cantidad={400}/>
-        <CardPieChart name='Electronicos' cantidad={278}/>
-        <CardPieChart name='Educativos' cantidad={100}/>
-        <CardPieChart name='Exteriores' cantidad={100}/>
-        <CardPieChart name='De mesa' cantidad={100}/>
-     </div>
+      <div className='dashboard__content'>
+        {stockData.map((category) => (
+          <CardPieChart key={category.category_id} name={category.category_name} cantidad={category.total_stock} />
+        ))}
+      </div>
      
-     <div className='dashboard__products'>
-     <SellingProducts
-              key={1}
-              name={'Hello Kitty'}
-              sales={100}
-            />
-     </div>
-     
+      <div className='dashboard__products'>
+        <SellingProducts
+          key={1}
+          name={'Hello Kitty'}
+          sales={100}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
